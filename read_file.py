dbd14ca4010332c247adce74f6517da59d987756
Julien Berson <julien.berson@epitech.eu>
2025-04-22 21:13:46 +0200

    feat: added date cleaning and duplicates too

--- read_file.py
+++ read_file.py
@@ -9,6 +9,7 @@ import csv
 from matplotlib import pyplot as plt
 import numpy as np
 import sys
+from dictionary_visualizer import visualiser_dictionnaire
 
 clean_names = [
     "BORDEAUX ST JEAN",
@@ -106,6 +107,8 @@ def clean_data(df: pd.DataFrame):
                     .fillna("")
                     .apply(lambda x: get_closest_match(str(x), clean_names))
                 )
+        elif column == "Date":
+            df[column] = df[column].fillna(method='ffill')
         else:
             df[column] = df[column].fillna(0)
 
@@ -123,10 +126,22 @@ def read_csv(csv_path):
     return df
 
 
-def append_to_dic(line, columns, dic):
+def append_to_dic(line, columns, dic, table_of_dup):
     name = line["Departure station"]
+    temp_tab = [line["Date"], line["Departure station"], line["Arrival station"]]
+    if name == "None" or temp_tab in table_of_dup:
+        return 0
+    table_of_dup.append(temp_tab)
     if name not in dic:
         dic[name] = {}
+    if "Arrival stations" not in dic[name]:
+        dic[name]["Arrival stations"] = []
+    else:
+        dic[name]["Arrival stations"].append(line["Arrival station"])
+    if "Dates" not in dic[name]:
+        dic[name]["Dates"] = []
+    else:
+        dic[name]["Dates"].append(temp_tab[0])
     for elt in columns:
         if elt not in dic[name]:
             dic[name][elt] = []
@@ -136,10 +151,11 @@ def append_to_dic(line, columns, dic):
 
 def get_data_tab(df: pd.DataFrame):
     dic = {}
+    table = []
     df_sorted = df.sort_values("Departure station")
     numeric_columns = df_sorted.select_dtypes(include=[np.number]).columns.tolist()
     for i in range(0, len(df_sorted)):
-        append_to_dic(df_sorted.iloc[i], numeric_columns, dic)
+        append_to_dic(df_sorted.iloc[i], numeric_columns, dic, table)
     return dic
 
 
@@ -147,7 +163,6 @@ def get_data_station(df: pd.DataFrame):
     dic_values = {}
     temp_dic = {}
     numeric_columns = df.select_dtypes(include=[np.number]).columns.tolist()
-    stations = df["Departure station"].tolist()
     df_grouped = df.groupby("Departure station")[numeric_columns].mean().reset_index()
     to_csv(df_grouped, path="grouped.csv")
     for elt in df_grouped["Departure station"]:
@@ -168,11 +183,13 @@ def show_data(df):
     )
 
     df_grouped = (
-        df.groupby("Departure station")["Average journey time"].mean().reset_index()
+        df.groupby("Departure station")["Number of cancelled trains"]
+        .mean()
+        .reset_index()
     )
 
     stations = df_grouped["Departure station"].tolist()
-    delays = df_grouped["Average journey time"].tolist()
+    delays = df_grouped["Number of cancelled trains"].tolist()
 
     plt.figure(figsize=(10, 6))
     plt.plot(stations, delays, marker="o")
@@ -204,6 +221,14 @@ def main():
 
     data_values_dic = get_data_station(df)
     data_values_tab = get_data_tab(df)
+    # show_data(df)
+
+    for elt in data_values_tab:
+        for i in range(0, len(data_values_tab[elt])):
+            print(f"City: {elt}", end="")
+            print(
+                f"{data_values_tab[elt]['Arrival stations'][i]} - {data_values_tab[elt]['Dates'][i]}"
+            )
 
 
 if __name__ == "__main__":
