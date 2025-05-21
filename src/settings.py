bf67287645ee92049ce8fcf995653b3910324898
Julien Berson <julien.berson@epitech.eu>
2025-05-21 12:09:21 +0200

    feat: added information for dates value, fix: temporarly removed dark mode

--- src/settings.py
+++ src/settings.py
@@ -2,12 +2,6 @@ import streamlit as st
 from src.home import apply_white_mode, apply_dark_mode
 
 def admin_page():
-
-    with st.sidebar:
-        dark_mode = st.checkbox("Activer le mode sombre", value=st.session_state.get("dark_mode", False))
-
-        # Enregistrer l'état dans session_state
-        st.session_state["dark_mode"] = dark_mode
-        dark_mode = st.session_state.get("dark_mode", False)
+    return 0
 
 admin_page()
