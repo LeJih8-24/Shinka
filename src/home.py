1ee6a8f09437e23eb31dad2afd007c6fb41a9aa4
Julien Berson <julien.berson@epitech.eu>
2025-05-21 21:07:59 +0200

    feat: added main stats for homepage

--- src/home.py
+++ src/home.py
@@ -1,6 +1,10 @@
 import streamlit as st
+from st_circular_progress import CircularProgress
 import pandas as pd
 import numpy as np
+import altair as alt
+from src.station_pages.unique_station_data import get_all_infos
+from get_data import read_csv
 
 def apply_dark_mode():
     st.markdown(
@@ -68,17 +72,42 @@ def graphiques():
     )
     st.map(map_data)
 
+def display_bar_chart(data, title, metric_name):
+    df = pd.DataFrame(data, columns=["Departure station", metric_name])
+    chart = alt.Chart(df).mark_bar().encode(
+        x=alt.X("Departure station", sort="-y"),
+        y=metric_name,
+        tooltip=["Departure station", metric_name]
+    ).properties(
+        title=title,
+        width=500,
+        height=400
+    )
+    st.altair_chart(chart, use_container_width=True)
+
 def home_page():
 
     st.title("Main page")
-    dark_mode = st.session_state.get("dark_mode", False)
+    stats = read_csv("cleaned_dataset.csv")
+    dic = get_all_infos(stats)
+    train_sched, train_canc, avg_journey_time = st.columns(3, border=True)
+    print(dic["Top 3 stations with most trains cancelled"], dic["Top 3 stations with most trains scheduled"], dic["Top 3 stations with most average journey time"])
+    with train_sched:
+        display_bar_chart(dic["Top 3 stations with most trains scheduled"], "Top Stations - Scheduled Trains", "Total scheduled trains")
+    with train_canc:
+        display_bar_chart(dic["Top 3 stations with most trains cancelled"], "Top Stations - Cancelled Trains", "Total cancelled trains")
+    with avg_journey_time:      
+        display_bar_chart(dic["Top 3 stations with most average journey time"], "Top Stations - Avg Journey Time", "Avg journey time (min)")
+    col1, col2 = st.columns(2, border=True)
+    with col1:
+        st.metric("Average journey time (hours)", round(dic["Average journey time"] / 60, 1))
+    with col2:
+        st.metric("Average delay (min)", dic["Average delay"])
+    with st.container():
+        st.subheader("Percentage of delays where delay > 60")
+        st.progress(min(dic["delay > 60"] / 100, 1.0))  # si c'est un pourcentage
+    st.subheader("Top Delay Cause")
+    st.info(f"Main cause of delay: **{dic['Biggest delay cause'][0]}**")
 
-    if dark_mode:
-        apply_dark_mode()
-    else:
-        apply_white_mode()
-    tableaux()
-    graphiques()
-        
 
 home_page()
